9ebe8f33d060c994280ecf8763eac4982e2a9d5a
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-09-18 13:25:14 +0200

    lunes 3

--- codigo/matriz.cpp
+++ codigo/matriz.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <ctime>
 #include <iostream>
+#include <stdexcept>
 
 class Matriz {
 	
@@ -9,6 +10,8 @@ class Matriz {
 	
 	public:
 		Matriz(int filas = 10, int cols = 10);
+		//Matriz(const Matriz &);
+		//Matriz & operator=(const Matriz &);
 		void imprimir();
 		Matriz operator+(const Matriz &m);		
 		~Matriz();	
@@ -38,14 +41,51 @@ void Matriz::imprimir(){
 	}
 }
 
+Matriz Matriz::operator+(const Matriz &m){
+	if ((this->filas != m.filas) || (this->cols != m.cols)){
+		throw std::invalid_argument("Son matrices de distinto tamaño ...");
+	}
+	
+	Matriz aux(m.filas, m.cols);
+	for (int i = 0 ; i < m.filas ; i++){
+		for (int j = 0 ; j < m.cols ; j++){
+			aux.datos[i][j] = m.datos[i][j] + this->datos[i][j];
+		}
+	}
+	//aux.imprimir();
+	//std::cout << std::endl;
+	
+	return aux;
+}
+
 Matriz::~Matriz(){
-	///
+	for (int i = 0 ; i < filas ; i++){
+		delete [] this->datos[i];
+		this->datos[i] = nullptr;
+	}
+		
+	delete [] this->datos;
+	this->datos = nullptr;
+	this->filas = 0;
+	this->cols = 0;
 }
 
 
 int main(){
 	std::srand(std::time(NULL));
 	
-	Matriz m1;
+	Matriz m1(3,3);
+	Matriz m2(3,3);
+	Matriz suma = m1 + m2;
+	
+	//suma = m1 + m2;	
 	m1.imprimir();
+	std::cout << std::endl;
+	
+	m2.imprimir();
+	std::cout << std::endl;
+
+	suma.imprimir();
+	std::cout << std::endl;
+
 }
