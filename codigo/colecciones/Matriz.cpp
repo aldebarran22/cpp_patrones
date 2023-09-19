47fcaff62140975dc090e242911c493e6bdcb07c
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-09-19 13:32:25 +0200

    martes_13_32

--- codigo/colecciones/Matriz.cpp
+++ codigo/colecciones/Matriz.cpp
@@ -9,20 +9,21 @@ Matriz::Matriz(int f, int c)
 	this->c = c;
 	
 	// crear la matriz de f x c y rellenar de numero aleatorios
-	std::vector<std::vector<int>> matriz(f, std::vector<int>(c));
+	std::vector<std::vector<int>> m(f, std::vector<int>(c));
 	for (int i = 0; i < f; i++) {
 		for (int j = 0; j < c; j++) {
 
-			matriz.at(i).at(j) = std::rand() % 100;
+			m.at(i).at(j) = std::rand() % 100;
 		}
 	}
+	this->matriz = m;
 }
 
 void Matriz::imprimir(){
 
 	for (int i = 0; i < f; i++) {
 		for (int j = 0; j < c; j++) {
-			std::cout << matriz[i][j] << " ";
+			std::cout << matriz.at(i).at(j) << " ";
 		}
 		std::cout << std::endl;
 	}
