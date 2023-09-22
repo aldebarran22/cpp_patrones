62f42b49273ee05fa41065b5e8861d2e492e7e0f
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-09-22 10:05:43 +0200

    vie-5

--- codigo/patrones_estructurales/Adapter/main.cpp
+++ codigo/patrones_estructurales/Adapter/main.cpp
@@ -2,11 +2,14 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 #include "VectorPlano.h"
+#include "VectorPlano2.h"
 
 int main(int argc, char** argv) {
 	VectorPlano vp(12,6);
+	VectorPlano2 vp2(2,44);
 	
-
-	
+	std::cout << vp.prod(vp2) << std::endl;
+	std::cout << vp2.prod(vp) << std::endl;
+		
 	return 0;
 }
